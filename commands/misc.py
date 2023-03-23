87d7fbcc8a24febadff9d2c041abfa682973c2c0
Rafael <rhirata.s@gmail.com>
2023-03-23 12:30:10 -0300

    Added test command

--- commands/misc.py
+++ commands/misc.py
@@ -10,3 +10,7 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Comando não encontrado")
+
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.message.text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
--- main.py
+++ main.py
@@ -12,16 +12,19 @@ logging.basicConfig(
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(config.API_KEY).build()
-    
+
     start_handler = CommandHandler('start', miscCommands.start)
     unknown_handler = MessageHandler(filters.COMMAND, miscCommands.unknown)
     help_handler = CommandHandler('help', miscCommands.help)
     roll_handler = CommandHandler('roll', rollCommands.roll)
+    test_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), miscCommands.test)
+
 
     application.add_handler(start_handler)
     application.add_handler(help_handler)
     application.add_handler(roll_handler)
 
+    application.add_handler(test_handler)
     application.add_handler(unknown_handler) #added last
 
     application.run_polling()
