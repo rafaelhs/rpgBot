b0f328c07b14ef959207fac4359a26391354b66a
Rafael <rhirata.s@gmail.com>
2023-04-04 22:44:06 -0300

    Test for restricting unauthorized access.

--- modules/misc.py
+++ modules/misc.py
@@ -1,7 +1,24 @@
 from telegram import *
 from telegram.ext import *
+from functools import wraps
+import config
 import constants
 
+
+
+def restricted(func):
+    @wraps(func)
+    async def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        chat_id = update.message.chat_id
+        print(f"user: {user_id}   chat: {chat_id}")
+        if user_id not in config.LIST_OF_USERS and chat_id not in config.LIST_OF_CHATS:
+            print(f"Unauthorized access denied for {user_id}.")
+            return
+        return await func(update, context, *args, **kwargs)
+    return wrapped
+
+@restricted
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=constants.START_TEXT)
 
